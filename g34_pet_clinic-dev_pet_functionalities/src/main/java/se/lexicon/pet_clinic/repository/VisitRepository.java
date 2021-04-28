f0d217f14b379f3b282bc5fb89579f6f3b8f129f
asha joseph <ashajo183@gmail.com>
2021-04-28 13:34:55 +0200

    VisitRepository interface created

--- g34_pet_clinic-dev_pet_functionalities/src/main/java/se/lexicon/pet_clinic/repository/VisitRepository.java
+++ g34_pet_clinic-dev_pet_functionalities/src/main/java/se/lexicon/pet_clinic/repository/VisitRepository.java
@@ -1,8 +1,9 @@
 package se.lexicon.pet_clinic.repository;
 
-public interface VisitRepository {
+public interface VisitRepository extends CrudRepository<Visit,String> {
     // todo: implement basic CRUD
-
+Visit findVisitByPetName(String name);
+Visit findVisitByPetTypeName(String petTypeName);
     //todo: select visit by pet name
     //todo: select visit by pet type name
 }
