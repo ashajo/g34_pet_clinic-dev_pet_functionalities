9da5364836396e571e643cb7838b499b48cfd4ae
asha joseph <ashajo183@gmail.com>
2021-04-28 10:51:42 +0200

    PetService interface created

--- g34_pet_clinic-dev_pet_functionalities/src/main/java/se/lexicon/pet_clinic/service/PetService.java
+++ g34_pet_clinic-dev_pet_functionalities/src/main/java/se/lexicon/pet_clinic/service/PetService.java
@@ -1,6 +1,12 @@
 package se.lexicon.pet_clinic.service;
 
 public interface PetService {
+PetDto save(PetDto dto);
+PetDto update(PetDto dto);
+List<PetDto>findAll();
+PetDto findById(String id);
+PetDto findByName(String name);
+PetDto findByPetTypeName(String name);
 
     // todo: define SAVE, UPDATE, DELETE, Find All, FIND BY ID, find By Name, find By PetTypeName, find By OwnerFirstNameAndLastName, find By OwnerTelephone
 
