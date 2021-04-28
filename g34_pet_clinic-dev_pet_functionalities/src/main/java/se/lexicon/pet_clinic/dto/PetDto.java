5f78869c04a642721984bc5762f051c722ee2bda
asha joseph <ashajo183@gmail.com>
2021-04-28 10:42:17 +0200

    PetDto created

--- g34_pet_clinic-dev_pet_functionalities/src/main/java/se/lexicon/pet_clinic/dto/PetDto.java
+++ g34_pet_clinic-dev_pet_functionalities/src/main/java/se/lexicon/pet_clinic/dto/PetDto.java
@@ -1,5 +1,13 @@
 package se.lexicon.pet_clinic.dto;
 
+@Data
 public class PetDto {
+    public class PetDto {
+        private String id;
+        private String name;
+        private LocalDate birthDate;
+        private PetType petType;
+        private Owner owner;
+
     // todo: implement PetDto
 }
